927fc1bd2fdf7d642b433f383432c6ef2570dbc7
Atli Marcher Pálsson <atli.marcher.palsson@advania.is>
2025-01-25 10:23:03 +0000

    Invoice on post on payment method

--- MLPos.Core/Model/PaymentMethod.cs
+++ MLPos.Core/Model/PaymentMethod.cs
@@ -5,4 +5,5 @@ public class PaymentMethod : Entity
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string Image { get; set; } = string.Empty;
+    public bool InvoiceOnPost { get; set; } = false;
 }
