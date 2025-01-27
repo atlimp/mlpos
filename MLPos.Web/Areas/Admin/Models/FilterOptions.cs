f6b056d69d6be4cfd1ca480dd45bb1aaa7218fd7
Atli Marcher Pálsson <atli.marcher.palsson@advania.is>
2025-01-27 22:57:53 +0000

    Filter for transaction view

---
+++ MLPos.Web/Areas/Admin/Models/FilterOptions.cs
@@ -0,0 +1,17 @@
+﻿using MLPos.Core.Enums;
+using MLPos.Core.Model;
+
+namespace MLPos.Web.Models
+{
+    public class FilterOptions
+    {
+        public IEnumerable<Customer> Customers { get; set; }
+        public long? SelectedCustomerId { get; set; }
+        public IEnumerable<PaymentMethod> PaymentMethods { get; set; }
+        public long? SelectedPaymentMethodId { get; set; }
+        public string? DateFrom { get; set; }
+        public string? DateTo { get; set; }
+        public Dictionary<int, string> StatusValues { get; set; }
+        public TransactionStatus Status { get; set; }
+    }
+}
